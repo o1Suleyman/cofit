d713d6a167773ae7e930be7cc612faf5c0f8c95c
Suleyman <o1s.suleyman@gmail.com>
2024-11-29 11:30:37 +0400

    Clear

--- app/getsessions/page.tsx
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@/utils/supabase/server";
-export default async function GetSessions() {
-    const supabase = await createClient();
-    const { data, error } = await supabase.from("sessions").select("*");
-    return <>{JSON.stringify(data)}</>;
-}
--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -4,15 +4,6 @@ import { InfoIcon } from "lucide-react";
 import { redirect } from "next/navigation";
 
 export default async function ProtectedPage() {
-  const supabase = await createClient();
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
-  if (!user) {
-    return redirect("/sign-in");
-  }
 
   return (
     <div className="flex-1 w-full flex flex-col gap-12">
@@ -22,16 +13,6 @@ export default async function ProtectedPage() {
           This is a protected page that you can only see as an authenticated
           user
         </div>
-      </div>
-      <div className="flex flex-col gap-2 items-start">
-        <h2 className="font-bold text-2xl mb-4">Your user details</h2>
-        <pre className="text-xs font-mono p-3 rounded border max-h-32 overflow-auto">
-          {JSON.stringify(user, null, 2)}
-        </pre>
-      </div>
-      <div>
-        <h2 className="font-bold text-2xl mb-4">Next steps</h2>
-        <FetchDataSteps />
       </div>
     </div>
   );
