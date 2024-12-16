3d1370c004b9d90d355c76bd52db427f40240a2b
Suleyman <o1s.suleyman@gmail.com>
2024-12-16 22:18:10 +0400

    Refactor homepage

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,11 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Index() {
-  return (<div className="flex flex-col grow pt-8 gap-5 items-center">
-    <h1 className="">Take the quiz and get your personalized results!</h1>
+  return (<div className="flex flex-col grow pt-8 gap-5 items-center mb-4">
+    {/* <h1 className="">Take the quiz and get your personalized results!</h1> */}
+    <Image src="/bg2.png" alt="Demo" width={1081} height={695}/>
     <Button asChild>
       <Link href="/quiz">Get started</Link>
     </Button>
-    <Image src="/bg.png" alt="Demo" width={1000} height={542}/>
   </div>);
 }
