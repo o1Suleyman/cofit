b678f9ae833e3a0bd73b61dab175cc15cffcec67
Suleyman <o1s.suleyman@gmail.com>
2024-12-16 22:22:28 +0400

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Index() {
   return (<div className="flex flex-col grow pt-8 gap-5 items-center mb-4">
     {/* <h1 className="">Take the quiz and get your personalized results!</h1> */}
-    <Image src="/bg2.png" alt="Demo" width={1081} height={695}/>
+    <Image src="/bg2.png" alt="Demo" width={950} height={610}/>
     <Button asChild>
       <Link href="/quiz">Get started</Link>
     </Button>
---
+++ app/quiz/quiz.tsx
@@ -0,0 +1,200 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { createClient } from "@/utils/supabase/client";
+
+const formSchema = z.object({
+  age: z
+    .coerce.number()
+    .min(16, { message: "Age must be at least 16" })
+    .max(80, { message: "Age must be less than 80" }),
+  gender: z.enum(["male", "female"]),
+  height: z.coerce.number().max(300, {
+    message: "Height must be less than 300",
+  }),
+  weight: z.coerce.number().max(300, {
+    message: "Weight must be less than 300",
+  }),
+  goal: z.enum(["lose weight", "gain muscle"]),
+  activitylevel: z.enum(["low", "medium", "high"]),
+});
+
+export default function Quiz() {
+  const supabase = createClient();
+  const router = useRouter();
+
+  useEffect(() => {
+    const redir = async () => {
+      const { data, error } = await supabase.auth.getSession();
+      if (data.session === null || error) {
+        router.push("/sign-in");
+      }
+    };
+    redir();
+  }, [router, supabase]);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      age: 0,
+      height: 0,
+      weight: 0,
+      gender: "male", // Explicit default value
+      goal: "lose weight",
+      activitylevel: "low",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <FormField
+          control={form.control}
+          name="gender"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Gender</FormLabel>
+              <Select
+                onValueChange={field.onChange}
+                value={field.value || "male"}
+              >
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select gender" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="male">Male</SelectItem>
+                  <SelectItem value="female">Female</SelectItem>
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="age"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Age</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="height"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Height (in centimeters)</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="weight"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Weight (in kilograms)</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="goal"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>What do you want to achieve?</FormLabel>
+              <Select
+                onValueChange={field.onChange}
+                value={field.value || "lose weight"} // Controlled value with fallback
+              >
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select goal" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="lose weight">Lose weight</SelectItem>
+                  <SelectItem value="gain muscle">Gain muscle</SelectItem>
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="activitylevel"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Activity Level</FormLabel>
+              <Select
+                onValueChange={field.onChange}
+                value={field.value || "low"} // Controlled value with fallback
+              >
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select activity level" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="low">Low</SelectItem>
+                  <SelectItem value="medium">Medium</SelectItem>
+                  <SelectItem value="high">High</SelectItem>
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
