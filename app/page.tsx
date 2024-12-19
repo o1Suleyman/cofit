02ff92d15d9f86812b1648426f158f9d2de2958e
Suleyman <o1s.suleyman@gmail.com>
2024-12-19 18:16:40 +0400

    Improve homepage

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Index() {
-  return (<div className="flex flex-col grow pt-8 gap-5 items-center mb-4">
+  return (<div className="flex flex-col grow pt-7 gap-2 items-center mb-4">
     {/* <h1 className="">Take the quiz and get your personalized results!</h1> */}
-    <Image src="/bg2.png" alt="Demo" width={950} height={610}/>
+    <Image src="/bg2.png" alt="Demo" width={820} height={525}/>
     <Button asChild>
       <Link href="/quiz">Get started</Link>
     </Button>
