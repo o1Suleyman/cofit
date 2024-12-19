74ddd7ed559ccbc90c2fe490db400fd209e66450
Suleyman <o1s.suleyman@gmail.com>
2024-12-19 19:51:48 +0400

    Logo for small screens

--- components/logo.tsx
+++ components/logo.tsx
@@ -3,22 +3,34 @@
 import Image from 'next/image';
 import { useTheme } from 'next-themes';
 import { useRouter } from 'next/navigation';
+import styles from './logo.module.css';
 
 function ThemedImage() {
   const { theme, resolvedTheme } = useTheme();
   const router = useRouter();
   const lightSrc = '/dark.png';
   const darkSrc = '/light.png';
+  const lightFaviconSrc= '/faviconlight.png';
+  const darkFaviconSrc = '/favicondark.png';
   const isDark = resolvedTheme === 'dark' || theme === 'dark';
-  return (
+  return (<>
     <Image
       src={isDark ? darkSrc : lightSrc}
       width={200}
       height={45}
       alt="Logo"
       onClick={() => router.push('/')}
-      className="cursor-pointer transition-opacity duration-300"
+      className={`${styles.logo} cursor-pointer transition-opacity duration-300`}
     />
+    <Image
+      src={isDark ? lightFaviconSrc : darkFaviconSrc}
+      width={50}
+      height={42}
+      alt="Logo"
+      onClick={() => router.push('/')}
+      className="cursor-pointer transition-opacity duration-300 md:hidden"
+    />
+    </>
   );
 }
 
