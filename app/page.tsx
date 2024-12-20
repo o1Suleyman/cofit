403d20297847c950e135743053f9171a069cd447
Suleyman <o1s.suleyman@gmail.com>
2024-12-20 18:51:07 +0400

    Change home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
 import { Button } from "@/components/ui/button";
+import { createClient } from "@/utils/supabase/server";
 import Image from "next/image";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
-export default function Index() {
+export default async function Index() {
+  const supabase = await createClient();
+  const { data } = await supabase.auth.getUser();
   return (<div className="flex flex-col grow pt-7 gap-2 items-center mb-4">
     {/* <h1 className="">Take the quiz and get your personalized results!</h1> */}
     <Image src="/bg2.png" alt="Demo" width={820} height={525}/>
-    <Button asChild>
+    {!data.user ? <Button asChild>
       <Link href="/quiz">Get started</Link>
-    </Button>
+    </Button> : <Button asChild>
+      <Link href="/dashboard">Dashboard</Link></Button>}
   </div>);
 }
