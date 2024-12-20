0cebaba8f1bb5274e04f3cea641f3b1f1d85c858
Suleyman <o1s.suleyman@gmail.com>
2024-12-20 19:27:51 +0400

    Semi

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,10 @@
 import { createClient } from "@/utils/supabase/server"
 import { redirect } from "next/navigation";
+import {
+    ResizableHandle,
+    ResizablePanel,
+    ResizablePanelGroup,
+  } from "@/components/ui/resizable"  
 
 export default async function DashboardPage() {
     const supabase = await createClient();
@@ -7,5 +12,31 @@ export default async function DashboardPage() {
     if (!data.user) {
         redirect("/sign-in");
     }
-    return (<div className="my-2 flex"></div>)
+    return (
+        <ResizablePanelGroup
+          direction="horizontal"
+          className="max-w-screen-lg rounded-lg border md:min-w-[450px] my-auto"
+        >
+          <ResizablePanel defaultSize={50}>
+            <div className="flex h-[50vh] items-center justify-center p-6">
+              <span className="font-semibold">One</span>
+            </div>
+          </ResizablePanel>
+          <ResizableHandle />
+          <ResizablePanel defaultSize={50}>
+            <ResizablePanelGroup direction="vertical">
+              <ResizablePanel defaultSize={25}>
+                <div className="flex h-full items-center justify-center p-6">
+                  <span className="font-semibold">Two</span>
+                </div>
+              </ResizablePanel>
+              <ResizableHandle />
+              <ResizablePanel defaultSize={75}>
+                <div className="flex h-full items-center justify-center p-6">
+                  <span className="font-semibold">Three</span>
+                </div>
+              </ResizablePanel>
+            </ResizablePanelGroup>
+          </ResizablePanel>
+        </ResizablePanelGroup>)
 }
