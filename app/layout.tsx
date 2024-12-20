2c3ee9824e1ef199a51f9b25d71e95fe05274743
Suleyman <o1s.suleyman@gmail.com>
2024-12-20 19:02:22 +0400

    Add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from "next-themes";
 import "./globals.css";
 import Logo from "@/components/logo";
 import NavMenu from "@/components/navmenu";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -41,6 +42,7 @@ export default function RootLayout({
               </nav>
               {/* <div className="max-w-5xl p-4"> */}
                 {children}
+                <SpeedInsights />
               {/* </div> */}
               <footer className="w-full flex items-center justify-center border-t mx-auto text-center gap-8 py-8 mt-auto">
                 <ThemeSwitcher />
