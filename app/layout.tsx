cefd6b31810e5da640dd8d5ff64eba050f6bd28b
Suleyman <o1s.suleyman@gmail.com>
2024-12-28 23:55:48 +0400

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import "./globals.css";
 import Logo from "@/components/logo";
 import NavMenu from "@/components/navmenu";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -42,6 +43,7 @@ export default function RootLayout({
               </nav>
               {/* <div className="max-w-5xl p-4"> */}
                 {children}
+                <Analytics />
                 <SpeedInsights />
               {/* </div> */}
               <footer className="w-full flex items-center justify-center border-t mx-auto text-center gap-8 py-8 mt-auto">
